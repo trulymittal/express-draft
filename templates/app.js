a1a02d63a68eb7f53d06fb3db3637ee93b1bb60d
Truly Mittal <trulymittal@gmail.com>
2021-03-29 16:00:40 +0530

    minor fix for port in log statement

--- templates/app.js
+++ templates/app.js
@@ -27,4 +27,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 @ http://localhost:3000`));
+app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
